10f98d605253db7c1ccbf9c6906a013ff0d990fc
terukazu inoue <terukazu.inoue@codianz.com>
2023-03-23 22:51:25 +0900

    Rough implementation of map operator

--- src/observable.rs
+++ src/observable.rs
@@ -19,11 +19,34 @@ impl Subscription {
   }
 }
 
+pub struct Emitter<Item>
+where
+  Item: Clone + Send + Sync + 'static,
+{
+  func: Box<dyn Fn(Arc<Observer<Item>>, Arc<RwLock<bool>>) + Send + Sync + 'static>,
+}
+
+impl<Item> Emitter<Item>
+where
+  Item: Clone + Send + Sync + 'static,
+{
+  pub fn new<F>(f: F) -> Emitter<Item>
+  where
+    F: Fn(Arc<Observer<Item>>, Arc<RwLock<bool>>) + Send + Sync + 'static,
+  {
+    Emitter { func: Box::new(f) }
+  }
+  pub fn execute(&self, observer: Arc<Observer<Item>>, is_subscribed: Arc<RwLock<bool>>) {
+    (self.func)(observer, is_subscribed);
+  }
+}
+
+#[derive(Clone)]
 pub struct Observable<Item>
 where
   Item: Clone + Send + Sync + 'static,
 {
-  source: Box<dyn Fn(Arc<Observer<Item>>, Arc<RwLock<bool>>)>,
+  source: Arc<Emitter<Item>>,
 }
 
 impl<Item> Observable<Item>
@@ -35,7 +58,7 @@ where
     Source: Fn(Arc<Observer<Item>>, Arc<RwLock<bool>>) + Send + Sync + 'static,
   {
     Observable {
-      source: Box::new(source),
+      source: Arc::new(Emitter::new(source)),
     }
   }
 
@@ -64,7 +87,7 @@ where
     let observer = Arc::new(observer);
     let unsub_observer = Arc::clone(&observer);
     let unsub_is_subscribed = Arc::clone(&is_subscribed);
-    (self.source)(observer, is_subscribed);
+    self.source.execute(observer, is_subscribed);
     Subscription::new(move || {
       unsub_observer.close();
       if let Ok(mut state) = unsub_is_subscribed.write() {
@@ -73,53 +96,56 @@ where
     })
   }
 
-  // pub fn map<Out, F>(&self, f: F) -> Observable<Out>
-  // where
-  //   F: Fn(Item) -> Out + Send + Sync + 'static,
-  //   Out: Clone + Send + Sync + 'static,
-  // {
-  //   struct FWrap<In, Out>
-  //   where
-  //     In: Clone + Send + Sync + 'static,
-  //     Out: Clone + Send + Sync + 'static,
-  //   {
-  //     func: Box<dyn Fn(In) -> Out + Send + Sync + 'static>,
-  //   }
-  //   impl<In, Out> FWrap<In, Out>
-  //   where
-  //     In: Clone + Send + Sync + 'static,
-  //     Out: Clone + Send + Sync + 'static,
-  //   {
-  //     fn new<F>(func: F) -> FWrap<In, Out>
-  //     where
-  //       F: Fn(In) -> Out + Send + Sync + 'static,
-  //       In: Clone + Send + Sync + 'static,
-  //       Out: Clone + Send + Sync + 'static,
-  //     {
-  //       FWrap {
-  //         func: Box::new(func),
-  //       }
-  //     }
-  //     fn call(&self, indata: In) -> Out {
-  //       (self.func)(indata)
-  //     }
-  //   }
+  pub fn map<Out, F>(&self, f: F) -> Observable<Out>
+  where
+    F: Fn(Item) -> Out + Send + Sync + 'static,
+    Out: Clone + Send + Sync + 'static,
+  {
+    struct FWrap<In, Out>
+    where
+      In: Clone + Send + Sync + 'static,
+      Out: Clone + Send + Sync + 'static,
+    {
+      func: Box<dyn Fn(In) -> Out + Send + Sync + 'static>,
+    }
+    impl<In, Out> FWrap<In, Out>
+    where
+      In: Clone + Send + Sync + 'static,
+      Out: Clone + Send + Sync + 'static,
+    {
+      fn new<F>(func: F) -> FWrap<In, Out>
+      where
+        F: Fn(In) -> Out + Send + Sync + 'static,
+        In: Clone + Send + Sync + 'static,
+        Out: Clone + Send + Sync + 'static,
+      {
+        FWrap {
+          func: Box::new(func),
+        }
+      }
+      fn call(&self, indata: In) -> Out {
+        (self.func)(indata)
+      }
+    }
 
-  //   let fwrap = Arc::new(FWrap::new(f));
-  //   Observable::<Out>::create(|s, _| {
-  //     xx.subscribe(
-  //       move |x| {
-  //         // s.next(fwrap.call(x));
-  //       },
-  //       |e| {
-  //         // s.error(e);
-  //       },
-  //       || {
-  //         // s.complete()
-  //       },
-  //     );
-  //   })
-  // }
+    let fwrap = Arc::new(FWrap::new(f));
+    let _self = self.clone();
+    Observable::<Out>::create(move |s, _| {
+      let s_next = Arc::clone(&s);
+      let s_error = Arc::clone(&s);
+      let s_complete = Arc::clone(&s);
+      let s_fwrap = Arc::clone(&fwrap);
+      _self.subscribe(
+        move |x| {
+          s_next.next(s_fwrap.call(x));
+        },
+        move |e| {
+          s_error.error(e);
+        },
+        move || s_complete.complete(),
+      );
+    })
+  }
 }
 
 mod test {
@@ -189,5 +215,42 @@ mod test {
     println!("started");
     thread::sleep(time::Duration::from_millis(1000));
     sbsc.unsubscribe();
+  }
+
+  #[test]
+  fn map() {
+    let o = Observable::<i32>::create(|s, _is_subscribed| {
+      for n in 0..10 {
+        s.next(n);
+      }
+      s.complete();
+    });
+
+    o.map(|x| x * 2).subscribe(
+      |x| println!("next {}", x),
+      |e| println!("error {:}", e),
+      || println!("complete"),
+    );
+  }
+
+  #[test]
+  fn map_thread() {
+    let o = Observable::<i32>::create(|s, _is_subscribed| {
+      let s = Arc::new(s);
+      thread::spawn(move || {
+        for n in 0..10 {
+          thread::sleep(time::Duration::from_millis(100));
+          s.next(n);
+        }
+        s.complete();
+      });
+    });
+
+    o.map(|x| format!("str {}", x)).subscribe(
+      |x| println!("next {}", x),
+      |e| println!("error {:}", e),
+      || println!("complete"),
+    );
+    thread::sleep(time::Duration::from_millis(1000));
   }
 }
